ca7c70d925cb8c4da127c46742ada84300bd605c
Raghavan S <raghavan2,s@tcs.com>
2024-08-17 22:14:33 +0530

    first commit

---
+++ src/test/java/com/edureka/hotelreservationsystem/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edureka.hotelreservationsystem.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
